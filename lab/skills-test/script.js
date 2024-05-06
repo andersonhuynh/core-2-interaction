bc910ed1379e3c5faa5bc431cca52229ef9d423a
anderson huynh <143528345+andersonhuynh@users.noreply.github.com>
2024-05-06 19:14:43 -0400

    color change

--- lab/skills-test/script.js
+++ lab/skills-test/script.js
@@ -13,3 +13,27 @@ console.log(add(1, 2));
 console.log(add(10, 10));
 console.log(add(100, -100));
 
+//Change circle color to Green//
+
+let circle= document.querySelector(".circle");
+let amIGreen = false;
+
+//Switch color from green to blue on click//
+
+circle.addEventListener("click", function(){
+    if(!amIGreen){
+        makeItGreen();
+        amIGreen = true;
+    } else if (amIGreen){
+        makeItBlue();
+        amIGreen = false;
+    }
+});
+
+function makeItGreen(){
+    circle.style.backgroundColor="green";
+}
+function makeItBlue(){
+    circle.style.backgroundColor="blue";
+}
+
